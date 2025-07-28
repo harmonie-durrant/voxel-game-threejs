807667b8b04ce69a8fc18c738c2893a800181fdb
Harmonie Durrant <harmoniedurrant@gmail.com>
2025-07-28 18:43:54 +0000

    Initial commit - Setup vite - Add threejs - Add renderer, camera & scene - Add basic cube - Add animation loop

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+
+// Renderer setup
+const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Camera setup
+const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+
+// Scene setup
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00d000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Render loop
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
