c7aa6a0fd8a759bed1d2988763483af81cec40dd
Harmonie Durrant <harmoniedurrant@gmail.com>
2025-07-29 18:23:52 +0000

    Add reset position key

--- src/player.ts
+++ src/player.ts
@@ -48,6 +48,10 @@ export class Player {
             case 'KeyD':
                 this.input.x = this.maxSpeed;
                 break;
+            case 'KeyR':
+                this.position.set(32, 16, 32);
+                this.velocity.set(0, 0, 0);
+                break;
             default:
                 break;
         }
