1a3d570e77310762f7c05ced39d801a21242def7
Harmonie Durrant <harmoniedurrant@gmail.com>
2025-07-29 12:40:33 +0000

    Fix texture filtering

--- src/blocks.ts
+++ src/blocks.ts
@@ -5,6 +5,8 @@ const textureLoader = new THREE.TextureLoader();
 function loadTexture(path : string) {
     const texture = textureLoader.load(path);
     texture.colorSpace = THREE.SRGBColorSpace;
+    texture.minFilter = THREE.NearestFilter;
+    texture.magFilter = THREE.NearestFilter;
     return texture;
 }
 
