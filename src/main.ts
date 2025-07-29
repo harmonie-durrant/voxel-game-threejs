945cf17a51a82c51cbca2fc4fe199e7d9083ec72
Harmonie Durrant <harmoniedurrant@gmail.com>
2025-07-29 08:22:59 +0000

    Refactor world logic

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { World } from './world';
 
 // Renderer setup
 const renderer = new THREE.WebGLRenderer();
@@ -18,6 +19,9 @@ controls.target.set(8, 0, 8);
 
 // Scene setup
 const scene = new THREE.Scene();
+const world = new World(16);
+world.generate();
+scene.add(world);
 
 function setupLights() {
   const light1 = new THREE.DirectionalLight();
@@ -33,26 +37,11 @@ function setupLights() {
   scene.add(ambiant);
 }
 
-// World "generation"
-function setupWorld(size: number) {
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
-  for (let x = 0; x < size; x++) {
-    for (let z = 0; z < size; z++) {
-      const cube = new THREE.Mesh(geometry, material);
-      cube.position.set(x, 0, z);
-      scene.add(cube);
-    }
-  }
-}
-
 // Render loop
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
 
-// Start functions
-setupWorld(16);
 setupLights();
 animate();
